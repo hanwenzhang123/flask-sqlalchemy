5895376965f89a7a361fd91213e6497786bb5a86
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-04-24 18:19:26 -0400

    Create 2-add-usermixin.py

---
+++ flask-app/2-add-usermixin.py
@@ -0,0 +1,21 @@
+pip install flask-login
+Use from flask_login import UserMixin instead of from flask.ext.login import UserMixin.
+
+A minxin is a acclass that gives some small in scope piece of functionality that is not standalone.
+
+
+#Import the UserMixin from Flask-Login. 
+#Remember that Flask extensions usually have import paths that start with flask.ext.
+#Now add UserMixin to the inheritance chain of the User model.
+
+import datetime
+
+from flask_login import UserMixin
+from peewee import *
+
+
+class User(UserMixin, Model):
+    email = CharField(unique=True)
+    password = CharField(max_length=100)
+    join_date = DateTimeField(default=datetime.datetime.now)
+    bio = TextField(default='')
