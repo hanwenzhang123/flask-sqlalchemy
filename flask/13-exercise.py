d4aa0ca7d60a015a1e25498d9686ce68f5002489
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-04-24 15:08:01 -0400

    Create 13-exercise.py

---
+++ flask/13-exercise.py
@@ -0,0 +1,10 @@
+#set-up module
+
+import datetime
+from peewee import *
+
+class User(Model):
+    email = CharField(unique=True)
+    password = CharField(max_length=100)
+    join_date = DateTimeField(default=datetime.datetime.now)
+    bio = TextField(default="")
