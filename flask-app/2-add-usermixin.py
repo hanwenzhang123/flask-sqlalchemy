3c971cad163d81a31aeda47ef8b108e6ac856e6f
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-04-24 18:21:56 -0400

    Update 2-add-usermixin.py

--- flask-app/2-add-usermixin.py
+++ flask-app/2-add-usermixin.py
@@ -2,7 +2,7 @@ pip install flask-login
 Use from flask_login import UserMixin instead of from flask.ext.login import UserMixin.
 
 A minxin is a acclass that gives some small in scope piece of functionality that is not standalone.
-https://flask-login.readthedocs.io/en/latest/#your-user-class
+//https://flask-login.readthedocs.io/en/latest/#your-user-class
 
 #Import the UserMixin from Flask-Login. 
 #Remember that Flask extensions usually have import paths that start with flask.ext.
