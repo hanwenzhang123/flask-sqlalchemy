54bd79b8e788dcd5df2c831b519ed6f64fb6b4ba
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-04-24 15:25:15 -0400

    Update 13-setup-module.py

--- flask/13-setup-module.py
+++ flask/13-setup-module.py
@@ -1,3 +1,12 @@
+#Import everything from the Peewee library. 
+#Create a new model named User. Give User an email attribute that is a CharField(). email should be unique.
+
+#Now add two more attributes/fields to User. The password field should be a CharField with a max_length of 100. 
+#And the join_date field should be a DateTimeField with a default value of datetime.datetime.now.
+
+#Finally, add a last field named bio that is a TextField. 
+#It should have an empty string for its default value. This makes it optional.
+
 import datetime
 from peewee import *
 
