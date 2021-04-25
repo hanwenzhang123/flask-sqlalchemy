f5fcd92e92393a476162f9129e8ddcabaf378f18
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-04-25 08:06:14 -0400

    Create 18-review-questions.py

---
+++ flask-app/18-review-questions.py
@@ -0,0 +1,15 @@
+When working with Flask-WTF and WTForms, what form method is used to automatically check if the data in the form is valid in a POST request?
+form.validate_on_submit()
+
+What field type is used to point to another record in the database?
+ForeignKeyField
+
+What function does Flask use to end a view early and throw an error?
+abort()
+
+I want to use Flask-Login with my User model. What should I add to the class definition?
+class User(UserMixin, Model):
+  
+Class methods take an argument that refers to the class itself. Generally this is named cls.
+
+Cryptographic hashing can not be reversed.
