d593cab0036f0f773107d7ea4e6c7612acc863ef
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-04-24 23:21:42 -0400

    Update 15-follow-unfollow.py

--- flask-app/15-follow-unfollow.py
+++ flask-app/15-follow-unfollow.py
@@ -4,7 +4,7 @@ def follow(username):
     try:
         to_user = models.User.get(models.User.username**username)
     except models.DoesNotExist:
-        abort(404)
+        pass
     else:
         try:
             models.Relationship.create(
@@ -23,7 +23,7 @@ def unfollow(username):
     try:
         to_user = models.User.get(models.User.username**username)
     except models.DoesNotExist:
-        abort(404)
+        pass
     else:
         try:
             models.Relationship.get(
@@ -35,3 +35,155 @@ def unfollow(username):
         else:
             flash("You've unfollowed {}!".format(to_user.username), "success")
     return redirect(url_for('stream', username=to_user.username))
+
+
+
+#exercise
+#Add a view named follow with a route of "/follow/<int:user_id>". It should be login_required. 
+#In the view, create a new Relationship with the current user as the from_user and the user with the provided ID as the to_user. Return a redirect to index.
+
+#Now create a view for unfollowing a user. It should be at "/unfollow/<int:user_id>", should require a login, and should also redirect to the index view. 
+#The view should find and delete the existing Relationship instance.
+
+
+import datetime
+
+from flask import Flask, g, render_template, flash, redirect, url_for
+from flask.ext.bcrypt import check_password_hash
+from flask.ext.login import LoginManager, login_user, current_user, login_required, logout_user
+
+import forms
+import models
+
+app = Flask(__name__)
+app.secret_key = 'this is our super secret key. do not share it with anyone!'
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(userid):
+    try:
+        return models.User.select().where(
+            models.User.id == int(userid)
+        ).get()
+    except models.DoesNotExist:
+        return None
+
+
+@app.before_request
+def before_request():
+    g.db = models.DATABASE
+    g.db.connect()
+    g.user = current_user
+    
+
+@app.after_request
+def after_request(response):
+    g.db.close()
+    return response
+
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    form = forms.SignUpInForm()
+    if form.validate_on_submit():
+        models.User.new(
+            email=form.email.data,
+            password=form.password.data
+        )
+        flash("Thanks for registering!") 
+    return render_template('register.html', form=form)
+  
+
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+    form = forms.SignUpInForm()
+    if form.validate_on_submit():
+        try:
+            user = models.User.get(
+                models.User.email == form.email.data
+            )
+            if check_password_hash(user.password, form.password.data):
+                login_user(user)
+                flash("You're now logged in!")
+            else:
+                flash("No user with that email/password combo")
+        except models.DoesNotExist:
+              flash("No user with that email/password combo")
+    return render_template('register.html', form=form)
+
+@app.route('/secret')
+@login_required
+def secret():
+    return "I should only be visible to logged-in users"
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+  
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/order', methods=('GET', 'POST'))
+def order_lunch():
+    form = forms.LunchOrderForm()
+    if form.validate_on_submit():
+        models.LunchOrder.create(
+            user=g.user._get_current_object(),
+            date=form.date.data,
+            order=form.order.data.strip()
+        )
+    return render_template('lunch.html', form=form)
+
+
+@app.route('/today')
+@login_required
+def today():
+    order = models.LunchOrder.select().where(
+        models.LunchOrder.date == datetime.date.today() &
+        models.LunchOrder.user == g.user._get_current_object()
+    ).get()
+    return render_template('today.html', order=order)
+  
+
+@app.route('/cancel_order/<int:order_id>')
+@login_required
+def cancel_order(order_id):
+    try:
+        order = models.LunchOrder.select().where(
+            id=order_id,
+            user=g.user._get_current_object()
+        ).get()
+    except models.DoesNotExist:
+        pass
+    else:
+        order.delete_instance()
+    return redirect(url_for('index'))
+
+
+@app.route("/follow/<int:user_id>")
+@login_required
+def follow(user_id):
+    to_user = models.User.get(models.User.id==user_id)
+    models.Relationship.create(
+        from_user=g.user._get_current_object(),
+        to_user=to_user
+    )
+    return redirect(url_for("index"))
+
+
+@app.route("/unfollow/<int:user_id>")
+@login_required
+def unfollow(user_id):
+    to_user = models.User.get(models.User.id==user_id)
+    models.Relationship.get(
+        from_user=g.user._get_current_object(),
+        to_user=to_user
+    ).delete_instance()
+    return redirect(url_for("index"))
