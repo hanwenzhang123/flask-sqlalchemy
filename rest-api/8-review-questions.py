ef2e479b79ae40451ea16a722405c948e0127698
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-04-25 23:16:14 -0400

    Create 8-review-questions.py

---
+++ rest-api/8-review-questions.py
@@ -0,0 +1,31 @@
+The Argon2 algorithm taxes both the CPU and memory of the attacker. Why is this effective?
+Eventually an attacker will run out of both of those resources.
+
+Using Flask Blueprints, how do you set the URL that comes before the Blueprint's routes?
+url_prefix
+
+What kind of auth accepts a username and password in plain text?
+Basic Auth
+
+What does marshaling do?
+Turns a code construct into something that is safe to transmit
+
+For updating records, there are two HTTP methods commonly used: PATCH and what?
+Put
+
+When adding an argument to RequestParser, which argument controls where data is searched for?
+Location
+
+When you create, update, or delete a record, you should include the 'Location' header so the user knows where to go.
+
+Flask Blueprints are not pluggable apps.
+
+Rate limiting is a useful method of preventing DDOS attacks.
+
+Why are tokens a useful way to handle authentication?
+They do not require a username and password
+They can be set to time out automatically, requiring a fresh session
+They can be signed so you know they are from your authentication mechanism
+They can contain encoded data to provide extra information about an authenticated user
+
+
